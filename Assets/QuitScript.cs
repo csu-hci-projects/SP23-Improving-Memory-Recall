0f2c910613a784769a0d29699886ba97179b885b
zrfuller <122941073+zrfuller@users.noreply.github.com>
2023-02-24 13:12:38 -0700

    Add files via upload

---
+++ Assets/QuitScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+    public void quitGame(){
+        Application.Quit();
+    }
+}
