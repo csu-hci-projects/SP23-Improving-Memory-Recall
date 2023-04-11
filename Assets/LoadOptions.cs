5b45e9361c089ced56e3420aebb4372cfa4bfa4f
Hunter Hoden <102103242+hunterhoden@users.noreply.github.com>
2023-04-11 13:00:05 -0600

    background Color and Font size

--- Assets/LoadOptions.cs
+++ Assets/LoadOptions.cs
@@ -6,21 +6,48 @@ using UnityEngine.UI;
 public class LoadOptions : MonoBehaviour
 {
     public Text text;
+    public Image background;
     public static Font myFont;
     public static bool bold;
     public static bool italicize;
     public static Color colorChoice;
+    public static Color backgroundColor;
+    public static int textFontSize;
+
+    public static bool optimalSettings;
+
+    public static Font optimalFont = Resources.Load<Font>("Font/NimbusRomNo9L-Reg");
+    public static bool optimalBold = false;
+    public static bool optimalItalic = false;
+    public static Color optimalBackgroundColor = Color.yellow;
+    public static Color optimalFontcolor = Color.red;
 
     // Start is called before the first frame update
     void Start()
     {
-        setTextColor();
-        setTextFont();
-        setTextStyle();
+        if (optimalSettings)
+        {
+            text.fontSize = 60;
+            text.color = optimalFontcolor;
+            text.fontStyle = FontStyle.Normal;
+            background.color = optimalBackgroundColor;
+        }
+        else
+        {
+            setTextColor();
+            setTextFont();
+            setTextStyle();
+            setTextSize();
+            setBackgroundColor();
+        }
     }
 
     public void setTextFont()
     {
+        if(myFont == null)
+        {
+            text.font = optimalFont;
+        }
         text.font = myFont;
     }
 
@@ -54,4 +81,27 @@ public class LoadOptions : MonoBehaviour
 
     }
 
+    public void setTextSize()
+    {
+        if (textFontSize == 0)
+        {
+            text.fontSize = 60;
+        }
+        else
+        {
+            text.fontSize = textFontSize;
+        }
+    }
+
+    public void setBackgroundColor()
+    {
+        if(backgroundColor == null)
+        {
+            background.color = Color.white;
+        }
+        else
+        {
+            background.color = backgroundColor;
+        }
+    }
 }
--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -10,6 +10,9 @@ public class OptionsMenu : MonoBehaviour
     public void fontType(Font font) { LoadOptions.myFont = font; }
     public void fontItalicized() { LoadOptions.italicize = true; Debug.Log("ITALICIZE"); }
     public void fontBold() { LoadOptions.bold = true; Debug.Log("BOLD"); }
+    public void fontSize(int fontSize) { LoadOptions.textFontSize = fontSize; }
+    public void optimized() { LoadOptions.optimalSettings = true; }
+
     public void fontColor(string colorName) 
     {
         if (colorName == "Red")
@@ -24,6 +27,22 @@ public class OptionsMenu : MonoBehaviour
         else if (colorName == "Yellow"){
             LoadOptions.colorChoice = Color.yellow;
             Debug.Log("YELLOW");
+        }
+    }
+
+    public void backgroundColor(string colorName)
+    {
+        if(colorName == "Yellow")
+        {
+            LoadOptions.backgroundColor = Color.yellow;
+        }
+        else if(colorName == "Red")
+        {
+            LoadOptions.backgroundColor = Color.red;
+        }
+        else if(colorName == "Grey")
+        {
+            LoadOptions.backgroundColor = Color.grey;
         }
     }
 
